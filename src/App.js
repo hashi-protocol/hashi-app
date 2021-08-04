554f5ea1964b4e3372d7e95418b2f60fa537ef93
MathisGD <delaunay.mathis@hotmail.fr>
2021-08-04 17:59:08 +0200

    Setup app: App update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js'
+import Bridge from './components/Bridge.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <div className="Content">
+          <Bridge />
+        </div>
       </header>
     </div>
   );
