5d77ba03d6daeed3044d9675ca1578ee078e4844
neeeekitos <nikita.a.terekhov@gmail.com>
2021-08-07 19:34:06 +0200

    theme switcher : removed comments & changed the position of the switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar.js'
 import Bridge from './components/Bridge.js'
 import { MetaMaskProvider } from 'metamask-react'
-import ThemeSwitch from "./components/ThemeSwitch";
 import React from "react";
 
 
@@ -12,7 +11,6 @@ function App() {
         <MetaMaskProvider>
             <header className="App-header">
                 <Navbar />
-                <ThemeSwitch/>
                 <div className="Content">
                     <Bridge />
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './navbar.module.css'
 import { ReactComponent as Logo } from '../static/bridge.svg';
 import EthereumButton from "./MetamaskButton";
+import ThemeSwitch from "./ThemeSwitch";
 
 class Navbar extends React.Component {
 
@@ -18,6 +19,7 @@ class Navbar extends React.Component {
             <div className={`${styles.navbar} navbar`} >
                 <Logo className={styles.icon} fill='var(--text-color)' />
                 <EthereumButton />
+                <ThemeSwitch/>
             </div >
         )
     }
