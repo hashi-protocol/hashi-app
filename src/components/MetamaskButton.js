7f01c39cdea0b719ffb7060fc73f528ff6242445
MathisGD <delaunay.mathis@hotmail.fr>
2021-08-19 23:12:53 +0200

    change ethereum button

--- src/components/MetamaskButton.js
+++ src/components/MetamaskButton.js
@@ -1,7 +1,6 @@
-import React  from "react";
-import {useMetaMask} from "metamask-react";
+import React from "react";
+import { useMetaMask } from "metamask-react";
 import Button from "./Button";
-import Typography from "./Typography";
 
 export default function EthereumButton() {
 
@@ -13,13 +12,13 @@ export default function EthereumButton() {
 
     if (status === "unavailable") ethStatus = <div>Please, install the MetaMask wallet</div>
 
-    if (status === "notConnected") ethStatus = <Button onClick={connect}>Connect to MetaMask</Button>
+    if (status === "notConnected") ethStatus = <div>Connect to MetaMask</div>
 
     if (status === "connecting") ethStatus = <div>Connecting...</div>
 
     if (status === "connected") ethStatus = <div>Connected account: {account}</div>
 
-    return <Typography variant="body1">{ethStatus}</Typography> ;
+    return <Button onClick={connect} variant="body1">{ethStatus}</Button>;
 
 
 }
