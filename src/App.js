be740bf8a82e8caea6a9cb895f90d69e41e25d6a
neeeekitos <nikita.a.terekhov@gmail.com>
2021-08-20 10:13:01 +0200

    fetching NFTs by address via an API, NFT cards UI, bugfixes & metamask balance

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar.js'
 import Bridge from './components/Bridge.js'
-import { MetaMaskProvider } from 'metamask-react'
-import React from "react";
-
-
-
-
+import { useMetaMask } from 'metamask-react'
+// import { TezosToolkit } from "@taquito/taquito";
 
 function App() {
+  // const [Tezos,] = useState<TezosToolkit>(
+  //   new TezosToolkit("https://api.tez.ie/rpc/granadanet")
+  // );
+  // const [, setContract] = useState<any>(undefined);
+  // const [, setPublicToken] = useState<string | null>("");
+  // const [wallet, setWallet] = useState<any>(null);
+  // const [, setUserAddress] = useState<string>("");
+  // const [, setUserBalance] = useState<number>(0);
+  // const [, setStorage] = useState<number>(0);
+  // const [,] = useState<boolean>(false);
+  // const [, setBeaconConnection] = useState<boolean>(false);
+  // const [,] = useState<string>("transfer");
 
+  const { ethereum, status, account } = useMetaMask();
+  const [balance, setBalance] = useState(0);
 
   return (
-    <div className="App">
-      <MetaMaskProvider>
+      <div className="App">
         <header className="App-header">
-          <Navbar />
+          <Navbar status={status} balance={balance}/>
           <div className="Content">
-            <Bridge />
+            <Bridge ethereum={ethereum} status={status} account={account} setBalance={setBalance}/>
           </div>
         </header>
-      </MetaMaskProvider>
-    </div>
+      </div>
   );
 }
-
 export default App;
