eaa6bf27de8dc48affdb3dbfe91e7cb892d2fb6e
MathisGD <delaunay.mathis@hotmail.fr>
2021-08-30 12:09:09 +0200

    Beacon: connexion

--- src/components/BeaconButton.js
+++ src/components/BeaconButton.js
@@ -1,17 +1,77 @@
 import React from "react";
 import Button from "./Button"
 
-function BeaconButton(props) {
-  return (
-    <div>
+class BeaconButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Tezos: this.props.Tezos,
+      wallet: this.props.wallet
+    }
+
+    this.setupBeacon = this.setupBeacon.bind(this);
+    this.getActiveAccount = this.getActiveAccount.bind(this);
+    this.isConnected = this.isConnected.bind(this);
+    this.disconnect = this.disconnect.bind(this);
+  }
+
+  async setupBeacon(wallet) {
+    try {
+      console.log("Requesting permissions...");
+      const permissions = await wallet.client.requestPermissions();
+      console.log("Got permissions:", permissions.address);
+    } catch (error) {
+      console.log("Got error:", error);
+    }
+  }
+
+  async getActiveAccount(wallet) {
+    const activeAccount = await wallet.client.getActiveAccount();
+    return activeAccount;
+  }
+
+  async isConnected(wallet) {
+    // The following code should always be run during pageload if you want to show if the user is connected.
+    const activeAccount = await wallet.client.getActiveAccount();
+    if (activeAccount) {
+      // User already has account connected, everything is ready
+      // You can now do an operation request, sign request, or send another permission request to switch wallet
+      console.log("Already connected:", activeAccount.address);
+      return true;
+    } else {
+      // The user is not connected. A button should be displayed where the user can connect to his wallet.
+      console.log("Not connected!");
+      return false;
+    }
+  }
+
+  async disconnect(wallet) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await wallet.clearActiveAccount();
+
+    try {
+      const account = await wallet.getPKH();
+      console.log("Active Account", account);
+    } catch {
+      console.log("No wallet connected");
+    }
+  }
+
+  render() {
+    return (
       <header>
-        {!props.connected && <Button onClick={props.connect}>Connect to a Tezos Wallet</Button>}
-        {props.connected && (
-          <Button onClick={props.disconnect}>Disconnect from {props.activeAccount?.address}</Button>
-        )}
-      </header>
-    </div>
-  );
+        {!false && <Button onClick={() => this.setupBeacon(this.props.wallet)}>Connect to a Tezos Wallet</Button>}
+        {
+          false && (
+            <Button onClick={() => this.disconnect(this.props.wallet)}>Disconnect from {this.getActiveAccount(this.props.wallet)}</Button>
+          )
+        }
+      </header >
+    )
+  }
 
 }
 
